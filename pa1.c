7915ff8248c15c910d5e0a1575634103c72a52f3
jjongdals <whddnjs9225@naver.com>
2024-03-31 22:15:09 +0900

    pipe function design and debugging

--- pa1.c
+++ pa1.c
@@ -17,6 +17,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 /***********************************************************************
  * run_command()
@@ -35,44 +37,127 @@ int run_command(int nr_tokens, char *tokens[])
 	if (strcmp(tokens[0], "exit") == 0) return 0; // exit일 경우
 	pid_t pid;
 	int status, result = 0;
-	// cd일 경우
-	if (strcmp(tokens[0], "cd") == 0) {
-		//디렉토리를 변경할 경우, 두번째 토큰에 path가 전달됨 따라서 dir 문자열에 token의 두번째 토큰값 전달
-		char *dir = tokens[1]; 
-		//cd나 cd ~ 일 경우 사용자의 홈디렉토리로 변경
-		if (tokens[1] == NULL || strcmp(tokens[1], "~") == 0) {
-			dir = getenv("HOME"); 
+	int nr_pipes = 0; //파이프의 갯수
+	//파이프의 위치를 저장하는 배열 생성, 동적으로 할당해줌, nr_tokens - 1 을 해준 이유는 파이프 이후 추가적인 명령어가 들어오기 때문
+	int *pipe_loc = malloc(sizeof(int) * (nr_tokens - 1));
+	// pipe가 있는지 체크하고 파이프의 위치를 반환
+	for(int i = 0; i < nr_tokens; i++) {
+		if (strcmp(tokens[i], "|") == 0) {
+			pipe_loc[nr_pipes++] = i;
 		}
-		//디렉토리 변경에 실패할 경우엔 -1 반환 아니면 1 반환
-		if (chdir(dir) != 0) { 
-			return -1; 
-		}
-		return 1; 
 	}
-	//fork의 반환을 pid에 저장
-	pid = fork();
-	//fork의 반환값이 0이라면, 자식 프로세스임
-	if(pid == 0){
-		//execvp의 반환값을 result에 저장, result가 -1이면 실패
-		result = execvp(tokens[0], tokens);
-		//execvp가 실패하면 에러 반환 및 부모 프로세스로 돌아감
-		if (result == -1) {
-			exit(1);	
+	//파이프가 있는 경우, pipe_loc의 길이가 0이 아니면 파이프 존재
+	if(nr_pipes > 0) {
+		int idx = 0; // execvp에 넣어줄 변수
+		// 파일 디스크립터 생성 (파이프의 개수만큼 동적으로 생성해줌)
+		int (*pipefd)[2] = malloc(2 * sizeof(int) * nr_pipes);
+		for (int i = 0; i < nr_pipes; i++) {
+			if(pipe(pipefd[i]) == -1) exit(1); // 파이프 반환값이 -1이면 실패!
+		}
+		//파이프가 있을 떄도 기본 동작은 같음
+		for(int i = 0; i <= nr_pipes; i++) {
+			//cd일 경우 체크
+			if (strcmp(tokens[idx], "cd") == 0) {
+				//디렉토리를 변경할 경우, 두번째 토큰에 path가 전달됨 따라서 dir 문자열에 token의 두번째 토큰값 전달
+				char *dir = tokens[idx + 1]; 
+				//cd나 cd ~ 일 경우 사용자의 홈디렉토리로 변경
+				if (tokens[idx + 1] == NULL || strcmp(tokens[idx + 1], "~") == 0) {
+					dir = getenv("HOME"); 
+				}
+				//디렉토리 변경에 실패할 경우엔 -1 반환 아니면 1 반환
+				if (chdir(dir) != 0) { 
+					return -1; 
+				}
+				return 1; 
+			}
+			pid = fork(); // 포크해서 자식 프로세스를 만들고
+			if(pid > 0) {
+				waitpid(pid, &status, 0);
+			} 
+			else if (pid == 0) {
+				//파이프 input pipefd[0]이 input임
+				if (i > 0) {
+					dup2(pipefd[i-1][0], STDIN_FILENO);
+				}
+				//파이프 output
+				if (i < nr_pipes) {
+					dup2(pipefd[i][1], STDOUT_FILENO);
+				}
+				//파이프 닫기
+				for (int j = 0; j < nr_pipes; j++) {
+					close(pipefd[j][0]);
+					close(pipefd[j][1]);
+				}
+				// execvp 실행을 위해 tokens 초기화 필수
+				if (i < nr_tokens) {
+					tokens[pipe_loc[i]] = NULL;
+				}
+				fprintf(stdout, "My PID : %d tokens : %s", pid, tokens[idx]);
+				result = execvp(tokens[idx], &tokens[idx]);
+				//execvp 반환값으로 성공여부 체크
+				if (result == -1) {
+					exit(1);
+				}
+				else {
+					exit(0);
+				}				
+			}
+			fprintf(stdout, "pid : %d\t token[%d] : %s\n", pid, idx, *(tokens + idx));
+			// 파이프 다음이 실행되기 때문에 idx는 토큰의 위치에서 +1
+			idx = pipe_loc[i] + 1;
+		}
+		// 메모리 할당
+		fprintf(stdout, "PROCESS NUM : %d\n", pid);
+		free(pipe_loc);
+		free(pipefd);
+		if(status == 0) {
+			return 1;
 		}
 		else {
-		// 성공하면 exit(0)으로 부모 프로세스에게 정보 반환
-			exit(0);
+			return -1;
+		}
+	}
+	//파이프가 존재하지 않는 경우
+	else {
+		// cd일 경우
+		if (strcmp(tokens[0], "cd") == 0) {
+			//디렉토리를 변경할 경우, 두번째 토큰에 path가 전달됨 따라서 dir 문자열에 token의 두번째 토큰값 전달
+			char *dir = tokens[1]; 
+			//cd나 cd ~ 일 경우 사용자의 홈디렉토리로 변경
+			if (tokens[1] == NULL || strcmp(tokens[1], "~") == 0) {
+				dir = getenv("HOME"); 
+			}
+			//디렉토리 변경에 실패할 경우엔 -1 반환 아니면 1 반환
+			if (chdir(dir) != 0) { 
+				return -1; 
+			}
+			return 1; 
+		}
+		//fork의 반환을 pid에 저장
+		pid = fork();
+		//fork의 반환값이 0이라면, 자식 프로세스임
+		if(pid == 0){
+			//execvp의 반환값을 result에 저장, result가 -1이면 실패
+			result = execvp(tokens[0], tokens);
+			//execvp가 실패하면 에러 반환 및 부모 프로세스로 돌아감
+			if (result == -1) {
+				exit(1);	
+			}
+			else {
+			// 성공하면 exit(0)으로 부모 프로세스에게 정보 반환
+				exit(0);
+			}
+		}	
+		//자식 프로세스가 끝날 때까지 부모 프로세스는 대기
+		wait(&status);
+		//stauts가 0이면 성공, 아니면 실패
+		if(status == 0) {
+			return 1;
+		}
+		else {
+			return -1;
 		}
 	}	
-	//자식 프로세스가 끝날 때까지 부모 프로세스는 대기
-	wait(&status);
-	//stauts가 0이면 성공, 아니면 실패
-	if(status == 0) {
-		return 1;
-	}
-	else {
-		return -1;
-	}
 	return -1;
 }
 /***********************************************************************
