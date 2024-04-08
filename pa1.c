449be7afe6e8f5774f26431bb715c798327b6b3a
jjongdals <whddnjs9225@naver.com>
2024-04-09 01:04:21 +0900

    alias structer design

--- pa1.c
+++ pa1.c
@@ -19,6 +19,18 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include "list_head.h"
+#include "parser.h"
+
+#define CHILD 0
+// alias 구현을 위한 구조체 선언, name은 사용자가 지정한 변수명, command는 대응되는 명령어
+typedef struct alias {
+	struct list_head list;
+	char *name;
+	char *command;
+} alias_entry;
+//stack 자료구조로 alias 사용
+LIST_HEAD(stack);
 
 /***********************************************************************
  * run_command()
@@ -36,91 +48,89 @@ int run_command(int nr_tokens, char *tokens[])
 {
 	if (strcmp(tokens[0], "exit") == 0) return 0; // exit일 경우
 	pid_t pid;
-	int status, result = 0;
+	int status, status2, result = 0;
+	// alias가 있다면 alias 처리
+	if(alias_cnt > 0) {
+		alias_entry * pos;
+		//먼저 alias 순회
+		list_for_each_entry(pos, &stack, list) {
+			// 일단 nr_tokens만큼 처리하고 여기서 alias로 대체된 문자열이 있다면 그걸 대신해서 실행
+			for (int i = 0; i < nr_tokens ; i++) {
+				//i번째 token이 alias로 지정한 이름과 같다면? 거기에 있는 command를 실행
+				if(strcmp(tokens[i], pos->name) == 0) {
+					//execute command는 alias의 명령어임 name 같은 걸 이걸로 대체
+					char *execute_command = malloc(strlen(pos->command) + 1);
+					strcpy(execute_command, pos->command);
+				}
+			}
+		}
+	}
+
 	int nr_pipes = 0; //파이프의 갯수
-	//파이프의 위치를 저장하는 배열 생성, 동적으로 할당해줌, nr_tokens - 1 을 해준 이유는 파이프 이후 추가적인 명령어가 들어오기 때문
-	int *pipe_loc = malloc(sizeof(int) * (nr_tokens - 1));
+	//파이프의 위치를 저장하는 배열 생성, 동적으로 할당해줌, nr_tokens만큼 동적할당 해줌
+	int pipe_loc = 0;
 	// pipe가 있는지 체크하고 파이프의 위치를 반환
 	for(int i = 0; i < nr_tokens; i++) {
 		if (strcmp(tokens[i], "|") == 0) {
-			pipe_loc[nr_pipes++] = i;
+			pipe_loc = i;
+			nr_pipes++;
 		}
 	}
-	//파이프가 있는 경우, pipe_loc의 길이가 0이 아니면 파이프 존재
+	//파이프가 있는 경우, nr_pipes가 0이 아니면 파이프 존재
 	if(nr_pipes > 0) {
-		int idx = 0; // execvp에 넣어줄 변수
-		// 파일 디스크립터 생성 (파이프의 개수만큼 동적으로 생성해줌 -> 파이프가 여러개 일수도 있기 때문에)
-		int (*pipefd)[2] = malloc(2 * sizeof(int) * nr_pipes);
-		for (int i = 0; i < nr_pipes; i++) {
-			if(pipe(pipefd[i]) == -1) exit(1); // 파이프 반환값이 -1이면 실패!
-		}
-		//파이프가 있을 떄도 기본 동작은 같음
-		for(int i = 0; i <= nr_pipes; i++) {
-			//cd일 경우 체크
-			if (strcmp(tokens[idx], "cd") == 0) {
-				//디렉토리를 변경할 경우, 두번째 토큰에 path가 전달됨 따라서 dir 문자열에 token의 두번째 토큰값 전달
-				char *dir = tokens[idx + 1]; 
-				//cd나 cd ~ 일 경우 사용자의 홈디렉토리로 변경
-				if (tokens[idx + 1] == NULL || strcmp(tokens[idx + 1], "~") == 0) {
-					dir = getenv("HOME"); 
-				}
-				//디렉토리 변경에 실패할 경우엔 -1 반환 아니면 1 반환
-				if (chdir(dir) != 0) { 
-					return -1; 
-				}
-				return 1; 
+		if (strcmp(tokens[0], "cd") == 0) {
+			//디렉토리를 변경할 경우, 두번째 토큰에 path가 전달됨 따라서 dir 문자열에 token의 두번째 토큰값 전달
+			char *dir = tokens[1]; 
+			//cd나 cd ~ 일 경우 사용자의 홈디렉토리로 변경
+			if (tokens[1] == NULL || strcmp(tokens[1], "~") == 0) {
+				dir = getenv("HOME"); 
 			}
-			pid = fork(); // 포크해서 자식 프로세스를 만들고
-			if (pid == 0) {
-				//파이프 input pipefd[0]이 input임
-				if (i > 0) {
-					dup2(pipefd[i-1][0], STDIN_FILENO);
-				}
-				//파이프 output
-				if (i < nr_pipes) {
-					dup2(pipefd[i][1], STDOUT_FILENO);
-				}
-				//파이프 닫기
-				for (int j = 0; j < i; j++) {
-					close(pipefd[j][0]);
-					close(pipefd[j][1]);
-				}
-				// execvp 실행을 위해 tokens 초기화 필수
-				if (i < nr_tokens) {
-					tokens[pipe_loc[i]] = NULL;
-				}
-				else {
-					tokens[nr_tokens] = NULL;
-				}
-				result = execvp(tokens[idx], &tokens[idx]);
-				//execvp 반환값으로 성공여부 체크
+			//디렉토리 변경에 실패할 경우엔 -1 반환 아니면 1 반환
+			if (chdir(dir) != 0) { 
+				return -1; 
+			}
+			return 1; 
+		}
+		int pipefd[2];
+		if (pipe(pipefd) == -1) exit(1);
+		tokens[pipe_loc] = NULL;
+		// 왼쪽 자식 프로세스를 포크해줌
+		pid_t left_pid = fork();
+		// 부모 먼저 실행
+		if (left_pid != CHILD) {
+			waitpid(left_pid, &status, 0); // wait pid (that executes left command)
+			close(pipefd[STDOUT_FILENO]);
+			// 오른쪽 포크해줌
+			pid_t right_pid = fork();
+			// 부모 프로세스 실행
+			if (right_pid != CHILD) {
+				waitpid(right_pid, &status2, 0);
+				close(pipefd[STDIN_FILENO]);
+			}
+			//오른쪽 자식 프로세스일 때
+			else if (right_pid == CHILD) {
+				dup2(pipefd[STDIN_FILENO], STDIN_FILENO);
+				result = execvp(tokens[pipe_loc+1], &tokens[pipe_loc+1]);
+				close(pipefd[STDIN_FILENO]);
 				if (result == -1) {
+					fprintf(stderr, "Unable to execute %s\n", tokens[pipe_loc+1]);
 					exit(1);
 				}
-				else {
-					exit(0);
-				}				
+				exit(0);
 			}
-			// 파이프 다음이 실행되기 때문에 idx는 토큰의 위치에서 +1
-			idx = pipe_loc[i] + 1;
 		}
-		// 부모 프로세스 파일 디스크립터 닫기
-		for (int i = 0; i < nr_pipes; i++) {
-			close(pipefd[i][0]);
-			close(pipefd[i][1]);
+		else if (left_pid == CHILD){
+			dup2(pipefd[STDOUT_FILENO], STDOUT_FILENO);
+			result = execvp(tokens[0], &tokens[0]);
+			close(pipefd[STDOUT_FILENO]);
+			if (result == -1) {
+				fprintf(stderr, "Unable to execute %s\n", tokens[0]);
+				exit(1);
+			}
+			exit(0);
 		}
-		// 자식 프로세스가 끝날 때까지 부모 프로세스는 대기
-		while (wait(&status) > 0);
-		// 메모리 할당 해제
-		free(pipe_loc);
-		free(pipefd);
-		if(status == 0) {
-			return 1;
-		}
-		else {
-			return -1;
-		}
-	}
+		return 1;
+	} 
 	//파이프가 존재하지 않는 경우
 	else {
 		// cd일 경우
@@ -136,6 +146,19 @@ int run_command(int nr_tokens, char *tokens[])
 				return -1; 
 			}
 			return 1; 
+		}
+		//alias 일 경우 내부 명령어기 때문에 fork 할 필요는 없음
+		if (strcmp(tokens[0], "alias") == 0) {
+			alias_entry * alias_list;
+			// alias에 저장된 명령어를 출력할 때readme에는 역순으로 출력함 따라서 reverse로 접근
+			list_for_each_entry_reverse(alias_list, &stack, list) {
+				fprintf(stderr, "%s: %s\n", alias_list->name, alias_list->command);
+			}
+			//alias 명령어를 추가하는 케이스도 생각해야 함 -> 이 때는 nr_tokens가 2개 이상임
+			if (nr_tokens > 1) {
+				// 이건 일단 내일 생각하자 ㅠㅠ~
+			}
+			return 1;
 		}
 		//fork의 반환을 pid에 저장
 		pid = fork();
